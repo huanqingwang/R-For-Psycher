a257786ab051c14d11e2bf6208a420df19eeb497
Huanqing Wang <wanghuanqing@mail.bnu.edu.cn>
2020-02-27 21:42:35 +0800

    Update Linear-Regression.R

--- Regression/Linear-Regression.R
+++ Regression/Linear-Regression.R
@@ -2,4 +2,5 @@
 Ylm<-lm(Y~X1+X2,data=name_of_dataset)
 GLM_summary(Ylm) # used to output the result 
 # Command 'lm' is used to conduct regression. The command format is dependent_variable~independent1+independent2, data=dataset.
+# Note: Y~X1+X2 is step-wise analyze.
 # Example of the output result is shown in 'Introduction.md'.
