d31dfd96dd1a866970b91130a637dfb957dbb09f
Huanqing Wang <wanghuanqing@mail.bnu.edu.cn>
2020-02-27 20:36:58 +0800

    Update and rename Linear-Regression to Linear-Regression.R

---
+++ Regression/Linear-Regression.R
@@ -0,0 +1,5 @@
+# We should use long format to conduct linear regression. Example of long format can be seen in 'Introduction.md'.
+Ylm<-lm(Y~X1+X2,data=name_of_dataset)
+GLM_summary(Ylm) # used to output the result 
+# Command 'lm' is used to conduct regression. The command format is dependent_variable~independent1+independent2, data=dataset.
+# Example of the output result is shown in 'Introduction.md'.
