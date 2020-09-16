a1e02608e45f147143355c554bf37bd77493e01b
Huanqing Wang <wanghuanqing@mail.bnu.edu.cn>
2020-09-16 09:46:09 +0800

    Update t-test R code.R

--- t-test/t-test R code.R
+++ t-test/t-test R code.R
@@ -3,7 +3,8 @@
 # independent sample t-test
 rm(list=ls()) #clear global environment
 library(xlsx)
-tfile=read.xlsx('D:/Wang Lab/R analysis/t-test/independent-sample_t-test.xlsx','Sheet1')
+#read file
+tfile=read.xlsx('t-test_sample.xlsx','Sheet1')
 t.test(tfile$Group1,tfile$Group2,data=tfile,paired = FALSE)
 # paired sample t-test
 t.test(tfile$Group1,tfile$Group2,data=tfile,paired = TRUE)
